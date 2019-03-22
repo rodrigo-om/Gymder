095b392902ed75e5fff0a2167211cb1aca699cbf
Rodrigo de Oliveira Magalhães <38271352+rodrigo-om@users.noreply.github.com>
2019-03-22 00:13:05 -0300

    Adiciona arquivos faltantes no root e mais um armazenamento no localStorage

--- app.js
+++ app.js
@@ -1,67 +1,69 @@
 (function() {
 	angular.module('myApp', ['ui.router','ngStorage'])
 	.controller('UserCtrl', ['$scope', '$localStorage', function($scope, $localStorage) {
-      $scope.isrounded = false;
-      $scope.$storage = $localStorage; 
+		$scope.isrounded = false;
+		$scope.$storage = $localStorage; 
+		$localStorage.dede="dede"
+		$scope.playlists = ["setembro.jpg", "summer.png", "trip-album-ART.jpg"];
+		$scope.selectedPlaylistIndex = -1; // Whatever the default selected index is, use -1 for no selection
+		$scope.locais = [{
+			id: 1,
+			label: 'Zona Sul'
+		}, {
+			id: 2,
+			label: 'Zona Norte'
+		}];
 
-      $scope.playlists = ["setembro.jpg", "summer.png", "trip-album-ART.jpg"];
-
-      $scope.selectedPlaylistIndex = 4; // Whatever the default selected index is, use -1 for no selection
-
-      $scope.coverClicked = function ($index) {
-         $scope.playlistSelected = $scope.selectedPlaylistIndex != $index
-         $scope.selectedPlaylistIndex = $scope.selectedPlaylistIndex != $index 
-         ? $scope.selectedPlaylistIndex = $index 
-         : $scope.selectedPlaylistIndex = 4;
-      };
-
-      $scope.store = function(storeName) {
-         $localStorage.d
-      }
-   }])
+		$scope.coverClicked = function ($index) {
+			$scope.playlistSelected = $scope.selectedPlaylistIndex != $index
+			$scope.selectedPlaylistIndex = $scope.selectedPlaylistIndex != $index 
+			? $scope.selectedPlaylistIndex = $index 
+			: $scope.selectedPlaylistIndex = 4;
+		};
+	}])
 	.config(($stateProvider, $urlRouterProvider) => {
-      $urlRouterProvider.otherwise('/');
-      $stateProvider
-      .state({
-        name: 'home',
-        url: '/',
-        templateUrl: 'home.html'
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+		.state({
+			name: 'home',
+			url: '/',
+			templateUrl: 'home.html'
             //template: '<h3>dedede uma figura</h3>'
-      })
-      .state('test', {
-        url: 'test',
-        url: '/test',
-        templateUrl: 'test.html'
-     })
-      .state('local', {
-        url: 'local',
-        url: '/local',
-        templateUrl: 'local.html'
-     })
-      .state('objetivo', {
-        url: 'objetivo',
-        url: '/objetivo',
-        templateUrl: 'objetivos.html'
-     })
-      .state('academia', {
-        url: 'academia',
-        url: '/academia',
-        templateUrl: 'academia.html'
-     })
-      .state('equipamento', {
-        url: 'equipamento',
-        url: '/equipamento',
-        templateUrl: 'equipamentos.html'
-     })
-      .state('spotify', {
-        url: 'spotify',
-        url: '/spotify',
-        templateUrl: 'spotify.html'
-     })
-      .state('resultado', {
-        url: 'resultado',
-        url: '/resultado',
-        templateUrl: 'resultado.html'
-     })
-   });
+        })
+		.state('test', {
+			url: 'test',
+			url: '/test',
+			templateUrl: 'test.html'
+		})
+		.state('local', {
+			url: 'local',
+			url: '/local',
+			templateUrl: 'local.html'
+		})
+		.state('objetivo', {
+			url: 'objetivo',
+			url: '/objetivo',
+			templateUrl: 'objetivos.html'
+		})
+		.state('academia', {
+			url: 'academia',
+			url: '/academia',
+			templateUrl: 'academia.html'
+		})
+		.state('equipamento', {
+			url: 'equipamento',
+			url: '/equipamento',
+			templateUrl: 'equipamentos.html'
+		})
+		.state('spotify', {
+			url: 'spotify',
+			url: '/spotify',
+			templateUrl: 'spotify.html'
+		})
+		.state('resultado', {
+			url: 'resultado',
+			url: '/resultado',
+			templateUrl: 'resultado.html'
+		})
+	});
 })();
