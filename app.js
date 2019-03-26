8640599e42930a4483512d3dccc6a14d4bf76148
Rodrigo de Oliveira Magalhães <38271352+rodrigo-om@users.noreply.github.com>
2019-03-25 23:58:29 -0300

    melhoria no sistema de resultado

--- app.js
+++ app.js
@@ -1,60 +1,98 @@
 (function() {
-	angular.module('myApp', ['ui.router','ngStorage'])
+	angular.module('myApp', ['ui.router', 'ngStorage'])
 	.controller('UserCtrl', ['$scope', '$localStorage', function($scope, $localStorage) {
 		$scope.isrounded = false;
-		$scope.$storage = $localStorage; 
+		$scope.$storage = $localStorage;
 		$scope.playlists = ["setembro.jpg", "summer.png", "trip-album-ART.jpg"];
-		$scope.selectedPlaylistIndex = -1; // Whatever the default selected index is, use -1 for no selection
-		$scope.locais = [{
-			id: 1,
-			label: 'Zona Sul'
-		}, {
-			id: 2,
-			label: 'Zona Norte'
-		}];
-		$scope.objetivos = [{
-			id: 1,
-			label: 'Perder Peso'
-		}, {
-			id: 2,
-			label: 'Ganhar massa muscular'
-		}, {
-			id: 3,
-			label: 'Treinamento funcional'
-		}];
+			$scope.selectedPlaylistIndex = -1; // Whatever the default selected index is, use -1 for no selection
+			$scope.locais = [{
+				id: 1,
+				label: 'Zona Sul'
+			}, {
+				id: 2,
+				label: 'Zona Norte'
+			}];
+			$scope.objetivos = [{
+				id: 1,
+				label: 'Perder Peso'
+			}, {
+				id: 2,
+				label: 'Ganhar massa muscular'
+			}, {
+				id: 3,
+				label: 'Treinamento funcional'
+			}];
 
-		$scope.exercicios = [ 
-		{"id":1, "name":"esteira"},
-		{"id":2, "name":"supino"},
-		{"id":3, "name":"bola"}
-		]
+			$scope.listaExercicios = [{
+				"id": 1,
+				"name": "esteira",
+				"locais": [
+					"academia"
+				],
+				"objetivos": [
+					1
+				]
+			}, {
+				"id": 2,
+				"name": "supino",
+				"locais": [
+					"academia"
+				],
+				"objetivos": [
+					2
+				]
+			}, {
+				"id": 3,
+				"name": "bola",
+				"locais": [
+					"academia",
+					"casa"
+				],
+				"objetivos": [
+					3
+				]
+			}]
 
-		$scope.exerciciosDoLocal = {
-			"casa": {"exercicios":[1,2,3]},
-			"false": {"exercicios":[2]}
-		}
+			$scope.exerciciosPorLocal = {
+				"casa": {
+					"exercicios": [1, 2, 3]
+				},
+				"false": {
+					"exercicios": [2]
+				}
+			}
 
-		$scope.coverClicked = function ($index) {
-			$scope.playlistSelected = $scope.selectedPlaylistIndex != $index
-			$scope.selectedPlaylistIndex = $scope.selectedPlaylistIndex != $index 
-			? $scope.selectedPlaylistIndex = $index 
-			: $scope.selectedPlaylistIndex = 4;
-		};
+			$scope.listaEquipamentos = [{
+				"id": 1,
+				"name": "bla"
+			}, {
+				"id": 2,
+				"name": "blabla"
+			}, {
+				"id": 3,
+				"name": "blablabla"
+			}]
 
-		$scope.objetivoEscolhido = function (objetivo) {
-			$scope.$storage.objetivo=objetivo
-			$scope.selectedGoal='true'
-		};
+			$scope.coverClicked = function($index) {
+				$scope.playlistSelected = $scope.selectedPlaylistIndex != $index
+				$scope.selectedPlaylistIndex = $scope.selectedPlaylistIndex != $index ?
+				$scope.selectedPlaylistIndex = $index :
+				$scope.selectedPlaylistIndex = 4;
+			};
 
-		$scope.filtrarExercicios = function () {
-			return $scope.exercicios.filter(isEven)
-		};
+			$scope.objetivoEscolhido = function(objetivo) {
+				$scope.$storage.objetivo = objetivo
+				$scope.selectedGoal = 'true'
+			};
 
-		function isEven(value) {
-			return $scope.exerciciosDoLocal[$localStorage.local].exercicios.includes(value.id)
-			
-		}
-	}])
+			$scope.filtrarExercicios = function() {
+				return $scope.listaExercicios.filter(filtrarPorLocalEObjetivo)
+			};
+
+			function filtrarPorLocalEObjetivo(value) {
+				return value.locais.includes($localStorage.local) && value.objetivos.includes($localStorage.objetivo.id)
+			}
+		}])
 	.config(($stateProvider, $urlRouterProvider) => {
 		$urlRouterProvider.otherwise('/');
 		$stateProvider
@@ -62,8 +100,8 @@
 			name: 'home',
 			url: '/',
 			templateUrl: 'home.html'
-            //template: '<h3>dedede uma figura</h3>'
-        })
+					//template: '<h3>dedede uma figura</h3>'
+				})
 		.state('test', {
 			url: 'test',
 			url: '/test',
