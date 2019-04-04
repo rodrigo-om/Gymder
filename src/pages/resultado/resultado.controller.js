8a17308d7688b261cf65e1e2b07088e6ad8bfd4d
Rodrigo de Oliveira Magalhães <38271352+rodrigo-om@users.noreply.github.com>
2019-04-04 01:03:28 -0300

    Correções de interface e adicionados todos os filtros de resultado

--- src/pages/resultado/resultado.controller.js
+++ src/pages/resultado/resultado.controller.js
@@ -4,17 +4,39 @@
 		var vm = this;
 		vm.local = $localStorage.local
 		vm.equipamentos = $localStorage.equipamentos
+		vm.zonaAcademia = $localStorage.zonaAcademia
+		vm.objetivo = $localStorage.objetivo
+		vm.playlist = $localStorage.playlist
 		vm.$onInit = $onInit
-
+		
 		function $onInit() {
 			vm.exercicios = vm.filtrarExercicios()
-			//vm.equipamentos = vm.filtrarEquipamentos()
-			//vm.academias = vm.filtrarAcademias()
+			vm.academias = vm.filtrarAcademias()
+			vm.artistas = vm.filtrarArtistas()
+			vm.equipamentos = vm.filtrarEquipamentos()
 		}
-
+		
 		vm.filtrarExercicios = function() {
-			vm.listaExercicios = vm.listaExercicios.filter(function (value) {
+			return vm.listaExercicios = vm.listaExercicios.filter(function (value) {
 				return value.locais.includes($localStorage.local) && value.objetivos.includes($localStorage.objetivo.id)
+			})
+		};
+
+		vm.filtrarAcademias = function() {
+			return vm.listaAcademias = vm.listaAcademias.filter(function (value) {
+				return value.zona == $localStorage.zonaAcademia.id
+			})
+		};
+
+		vm.filtrarArtistas = function() {
+			return vm.listaArtistas = vm.listaArtistas.filter(function (value) {
+				return value.playlist == $localStorage.playlist
+			})
+		};
+
+		vm.filtrarEquipamentos = function() {
+			return vm.listaEquipamentos = vm.listaEquipamentos.filter(function (value) {
+				return value.objetivos.includes($localStorage.objetivo.id)
 			})
 		};
 
@@ -45,7 +67,8 @@
 			],
 			"objetivos": [
 				3
-			]
+			],
+			"equipamento": true
 		}]
 
 		vm.exerciciosPorLocal = {
@@ -60,13 +83,50 @@
 		vm.listaEquipamentos = [{
 			"id": 1,
 			"name": "bla",
-			
+			"objetivos": [
+				1
+			]
 		}, {
 			"id": 2,
-			"name": "blabla"
+			"name": "blabla",
+			"objetivos": [
+				2
+			]
 		}, {
 			"id": 3,
-			"name": "blablabla"
+			"name": "blablabla",
+			"objetivos": [
+				3
+			]
 		}]
+
+		vm.listaAcademias = [{
+			"id": 1,
+			"name": "Smart Fit",
+			"zona": 2
+		}, {
+			"id": 2,
+			"name": "Arena",
+			"zona": 1
+		}, {
+			"id": 3,
+			"name": "Shape",
+			"zona": 1
+		}]
+
+		vm.listaArtistas = [{
+			"id": 1,
+			"name": "Metallica",
+			"playlist": 2
+		}, {
+			"id": 2,
+			"name": "Katy Perry",
+			"playlist": 1
+		}, {
+			"id": 3,
+			"name": "Lady Gaga",
+			"playlist": 1
+		}]
+
     }]);
 }())
